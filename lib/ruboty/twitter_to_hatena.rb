7c622f4afcc99af354ab3cdf4878a9f8a6fee901
takus <info@takus.info>
2016-10-18 04:21:11 +0000

    Add function of post_between.

--- lib/ruboty/twitter_to_hatena.rb
+++ lib/ruboty/twitter_to_hatena.rb
@@ -1,5 +1,5 @@
 require "ruboty/twitter_to_hatena/version"
 require "ruboty/handlers/twitter_to_hatena"
 require "ruboty"
-require "edit_daily_tweets"
+require "edit_tweets"
 require "post_to_hatena"
--- lib/ruboty/twitter_to_hatena/actions/post_between.rb
+++ lib/ruboty/twitter_to_hatena/actions/post_between.rb
@@ -1,27 +1,23 @@
 module Ruboty
   module Twitter_To_Hatena
     module Actions
-      class Post < Ruboty::Actions::Base
+      class PostBetween < Ruboty::Actions::Base
         def call
-          message.reply(post)
+          message.reply(post_between)
         rescue => e
           message.reply(e.message)
         end
 
         private
-        def post
-          yesterday = (Date.today - 1).to_s
-          if message[:date].empty?
-            date = yesterday
-          else
-            date = message[:date]
-          end
+        def post_between
+          start_date = message[:start_date]
+          end_date = message[:end_date]
 
-          tweets = EditTweets.fetch_daily_tweets(date)
+          tweets = EditTweets.fetch_tweets_between(start_date, end_date)
           return "There aren't tweets." if tweets.empty?
 
           body = EditTweets.edit_tweets_for_hatena(tweets)
-          if PostToHatena.post_to_hatena("つぶやき on #{date}", body, "twitter")
+          if PostToHatena.post_to_hatena("つぶやき between #{start_date} and #{end_date}", body, "twitter")
             "Post tweets to hatena."
           else
             "Posting tweets to hatena fails."
