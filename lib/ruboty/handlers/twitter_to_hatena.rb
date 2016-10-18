7ae51edd7e1c08c0ec09e05018bb651352030477
takus <info@takus.info>
2016-10-18 05:43:55 +0000

    Modify handler

--- lib/ruboty/handlers/twitter_to_hatena.rb
+++ lib/ruboty/handlers/twitter_to_hatena.rb
@@ -5,7 +5,7 @@ module Ruboty
   module Handlers
     # Post tweets to hatena blog.
     class Twitter_To_Hatena < Base
-      on /post_between (?<start_date>) (?<end_date>)\z/, name: 'post_between', description: 'Post tweets to hatena blog between YYYY/MM/DD YYYY/MM/DD.'
+      on /post_between (?<start_date>.+) (?<end_date>.+)\z/, name: 'post_between', description: 'Post tweets to hatena blog between YYYY/MM/DD YYYY/MM/DD.'
       on /post (?<date>.+)\z/, name: 'post', description: 'Post tweets to hatena blog on YYYY/MM/DD.'
       on /post(?<date>)\z/, name: 'post', description: 'Post yesterday\'s tweets to hatena blog.'
 
